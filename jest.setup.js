9a4c7554be29a98faaa5a73f255fc413091cfa3b
Sarazz-san <perauld7@gmail.com>
2026-06-28 15:04:50 +0100

    feat: prepare android release build

--- jest.setup.js
+++ jest.setup.js
@@ -7,3 +7,33 @@ jest.mock('react-native-linear-gradient', () => {
 });
 
 jest.mock('react-native-gesture-handler', () => ({}));
+
+jest.mock('react-native-image-picker', () => ({
+  launchCamera: jest.fn(),
+  launchImageLibrary: jest.fn(),
+}));
+
+jest.mock('react-native-document-picker', () => ({
+  types: {
+    images: 'images',
+    audio: 'audio',
+  },
+  pickSingle: jest.fn(),
+  isCancel: jest.fn(),
+}));
+
+jest.mock('react-native-view-shot', () => ({
+  captureRef: jest.fn(),
+}));
+
+jest.mock('react-native-audio-recorder-player', () => {
+  return {
+    default: {
+      startRecorder: jest.fn(),
+      stopRecorder: jest.fn(),
+      addRecordBackListener: jest.fn(),
+      removeRecordBackListener: jest.fn(),
+      mmssss: jest.fn(() => '00:00:00'),
+    },
+  };
+});
